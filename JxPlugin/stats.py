e5242883539392eb2446f7b8eae5c7efa76d802e
Olivier Binda <olivier.binda@wanadoo.fr>
2009-07-16 14:47:58 +0200

    Initial support for exporting

--- JxPlugin/stats.py
+++ JxPlugin/stats.py
@@ -4,7 +4,9 @@
 # ---------------------------------------------------------------------------
 # This file is a plugin for the "anki" flashcard application http://repose.cx/anki/
 # ---------------------------------------------------------------------------
+from os import path
 from ankiqt import mw
+from ankiqt.ui.utils import getSaveFile, askUser
 
 ######################################################################
 #
@@ -69,9 +71,9 @@ def SeenHtml(Map,Query):
 	Dict=Map["Dict"]
 	Seen = {}
 	Color = {0:True}
-	Buffer = {0:""}
+	Buffer = {0:u""}
 	for value in Dict.values():
-		Buffer[value] = ""
+		Buffer[value] = u""
 		Color[value] = True	
 	for Stuff in mw.deck.s.column0(Query):
 		if Stuff not in Seen:
@@ -82,15 +84,15 @@ def SeenHtml(Map,Query):
 			Seen[Stuff] = 0
 			Color[value] = not(Color[value])			
 			if Color[value]:
-				Buffer[value] += Stuff
+				Buffer[value] += u"""<a style="text-decoration:none;color:black;" href=py:JxAddo(u"%(Stuff)s")>%(Stuff)s</a>""" % {"Stuff":Stuff}#Stuff
 			else:
-				Buffer[value] += """<span style="color:blue">"""+ Stuff +"""</span>"""
+				Buffer[value] += u"""<a style="text-decoration:none;color:blue;" href=py:JxAddo(u"%(Stuff)s")>%(Stuff)s</a>""" % {"Stuff":Stuff}#"""<span style="color:blue">"""+ Stuff +"""</span>"""
 	HtmlBuffer = ""
 	for key, string in Buffer.iteritems():
 		if key == 0:
-			HtmlBuffer += """<h2  align="center">Other</h2><p><font size=+2>%s</font></p>""" % string
+			HtmlBuffer += u"""<h2  align="center">Other</h2><p><font size=+2>%s</font></p>""" % string
 		else:
-			HtmlBuffer += """<h2  align="center">%s</h2><p><font size=+2>%s</font></p>""" % (Map["Legend"][key],string)
+			HtmlBuffer += u"""<h2  align="center">%s</h2><p><font size=+2>%s</font></p>""" % (Map["Legend"][key],string)
 	return HtmlBuffer
 
 def MissingHtml(Map,Query):
@@ -102,22 +104,56 @@ def MissingHtml(Map,Query):
 	Color = {0:True}
 	Buffer = {0:""}
 	for value in Dict.values():
-		Buffer[value] = ""
+		Buffer[value] = u""
 		Color[value] = True	
 	for Stuff,Note in Dict.iteritems():
 		if Stuff not in Seen:
 			Color[Note] = not(Color[Note])			
 			if Color[Note]:
-				Buffer[Note] += Stuff
+				Buffer[Note] += u"""<a style="text-decoration:none;color:black;" href=py:JxAddo(u"%(Stuff)s")>%(Stuff)s</a>""" % {"Stuff":Stuff}
 			else:
-				Buffer[Note] += """<span style="color:blue">"""+ Stuff +"""</span>"""
-	HtmlBuffer = ""
+				Buffer[Note] += u"""<a style="text-decoration:none;color:blue;" href=py:JxAddo(u"%(Stuff)s")>%(Stuff)s</a>""" % {"Stuff":Stuff}
+	HtmlBuffer = u""
 	for key, string in Buffer.iteritems():
 		if key != 0:
-			HtmlBuffer += """<h2  align="center">%s</h2><p><font size=+2>%s</font></p>""" % (Map["Legend"][key],string)
+			HtmlBuffer += u"""<h2  align="center">%s</h2><p><font size=+2>%s</font></p>""" % (Map[u"Legend"][key],string)
 	return HtmlBuffer	
+	
+User = []
+
+def JxAddo(Stuff):
+	User.append(Stuff)
+	JxShow()
+
+def JxClear():
+	User[0:] = []
+	JxShow()
+	
+def JxShow():
+	JxHtml = u"""<style> li {font-size:x-large;}</style>
+	<center><a href=py:Clear>Clear</a>&nbsp;&nbsp;<a href=py:Export>Export</a></center>
+	<ul>"""	
+	for Entry in User:
+		JxHtml += u"""<li>%s</li>""" % Entry  
+	JxHtml += u"""</ul>"""
+	py = {u"Export":JxExport,u"Clear":JxClear}
+	mw.help.showText(JxHtml,py)
 
 
 
-
-
+def JxExport():
+	JxPath = unicode(getSaveFile(mw,  _("Choose file to export to"), "","Tab separated text file (.csv)", ".csv"))
+	if not JxPath:
+		return
+	if not JxPath.lower().endswith(".csv"):
+		JxPath += ".csv"
+	if path.exists(JxPath):
+		# check for existence after extension
+		if not ui.utils.askUser("This file exists. Are you sure you want to overwrite it?"):
+		       return
+	out = ""
+	for Entry in User:
+		out += Entry + "\t" +"ol" +"\n"
+	JxFile = open(JxPath, "wb")
+	JxFile.write(out.encode("utf-8"))
+	JxFile.close()
