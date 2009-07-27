7d424d9d32ed0d2f00c7cb40da8a1cad0ac7dddd
Olivier Binda <olivier.binda@wanadoo.fr>
2009-07-27 20:48:29 +0200

    more code clean up

---
+++ JxPlugin/lowlevel.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright: Olivier Binda <olivier.binda@wanadoo.fr>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# ---------------------------------------------------------------------------
+# This file is a plugin for the "anki" flashcard application http://repose.cx/anki/
+# ---------------------------------------------------------------------------
+import codecs
+
+def JxReadFile(File):
+	"""Reads a tab separated text file and returns a list of tuples."""
+	List = []
+	File = codecs.open(File, "r", "utf8")
+	for Line in File:
+		List.append(tuple(Line.strip('\n').split('\t')))
+	File.close()
+	return List
