22a7adfa9b5ea77145f040f6f5f1e5598dcbff59
Olivier Binda <olivier.binda@wanadoo.fr>
2009-07-31 20:13:36 +0200

    added 1 needed file

---
+++ JxPlugin/default.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright: Olivier Binda <olivier.binda@wanadoo.fr>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# ---------------------------------------------------------------------------
+# This file is a plugin for the "anki" flashcard application http://repose.cx/anki/
+# ---------------------------------------------------------------------------
+Jx__Entry_Source_Target__Default = [
+(u"Word recall","""%(Reading)s""","""${Css}<div style="float:left"><div>${T2JLPT}</div><div>${T2Freq}</div></div><div><center>${Expression}<br />${Reading}</center></div>"""),
+(u"Word recognition","""%(Reading)s<br>%(Meaning)s""","""${Css}<div style="float:left;"><div>${T2JLPT}</div><div>${T2Freq}</div></div><div><center>${Reading}<br \>${Meaning}</center></div>"""),
+(u"Kanji character","""%(Kanji)s""","""${Css}<div style="float:left">${Stroke}<div>${K2JLPT}</div><div>${K2Jouyou}</div><div>${K2Freq}</div></div><center>${K2Words}</center>"""),
+(u"Kanji meaning","""%(Meaning)s""","""${Css}<div style="float:left">${Stroke}<div>${K2JLPT}</div><div>${K2Jouyou}</div><div>${K2Freq}</div></div><center>${Meaning}</center><center>${K2Words}</center>"""),
+(u"Kanji readings","""%(OnYomi)s<br>%(KunYomi)s""","""${Css}<div style="float:left">${Stroke}<div>${K2JLPT}</div><div>${K2Jouyou}</div><div>${K2Freq}</div></div><center>${OnYomi}<br />${KunYomi}</center><center>${K2Words}</center>""")]
