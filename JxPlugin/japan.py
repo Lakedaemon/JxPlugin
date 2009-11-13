c0a7e4b3e158c8420ac5079ee8d3657b59d7f31e
Lakedaemon <olivier.binda@wanadoo.fr>
2009-11-13 13:10:24 +0100

    Fix for mecab on os x

--- JxPlugin/japan.py
+++ JxPlugin/japan.py
@@ -45,7 +45,7 @@ class MecabControl(object):
             os.environ['PATH'] += (";%s\\mecab\\bin" % dir)
             os.environ['MECABRC'] = ("%s\\mecab\\etc\\mecabrc" % dir)
         elif sys.platform.startswith("darwin"):
-            dir = os.path.dirname(os.path.abspath(__file__))
+            dir = os.path.dirname(os.path.abspath(__file__/../japanese))
             os.environ['PATH'] += ":" + dir + "/osx/mecab/bin"
             os.environ['MECABRC'] = dir + "/osx/mecab/etc/mecabrc"
             os.environ['DYLD_LIBRARY_PATH'] = dir + "/osx/mecab/bin"
